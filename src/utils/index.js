870a88a94119b2018d164d4e4b35e989dd695884
Ahmaderta Rizqi <ahmadderta.rizqi30@gmail.com>
2020-09-24 22:10:31 +0700

    detail update

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,12 @@ export const timeConvert = (time) => {
    const hours = Math.floor(time / 60);
    const minutes = time % 60;
    return `${hours} hr ${minutes} mins`;
-}
+};
+
+export const findValueByJob = (nameValue, dataArray) => {
+   for(let i = 0; i < dataArray.length; i++) {
+      if(dataArray[i].job === nameValue) {
+         return dataArray[i];
+      }
+   }
+};
--- src/views/movies/MovieDetail.js
+++ src/views/movies/MovieDetail.js
@@ -4,7 +4,7 @@ import API from 'services/movies';
 import { imgURL } from 'constants/index';
 import * as Icon from 'react-feather';
 import dayjs from 'dayjs';
-import { timeConvert } from 'utils'
+import { findValueByJob, timeConvert } from 'utils'
 
 export default function MovieDetail(props) {
    const { movieID } = useParams();
@@ -19,13 +19,48 @@ export default function MovieDetail(props) {
       getMovieDetail();
    }, [movieID]);
 
-   console.log(movieDetail);
-   
+   const renderCredits = () => {
+      if (movieDetail.credits) {
+         const director = findValueByJob('Director', movieDetail.credits.crew);
+         const writer = findValueByJob('Writer', movieDetail.credits.crew);
+         return (
+            <React.Fragment>
+               <div className="info-credits">
+                  <div className="item">
+                     <h6 className="text-title is-size-6">Director</h6>
+                     <p>{director ? director.name : '--'}</p>
+                  </div>
+                  <div className="item">
+                     <h6 className="text-title is-size-6">Writer</h6>
+                     <p>{writer ? writer.name : '--'}</p>
+                  </div>
+               </div>
+            </React.Fragment>
+         );
+      }
+   };
+
+   const renderProductions = () => {
+      if(movieDetail.production_companies) {
+         return (
+            <React.Fragment>
+               <h6 className="text-title is-size-6">Production Companies</h6>
+               <p>
+                  {movieDetail.production_companies.length > 0 ? 
+                     movieDetail.production_companies.map(company => company.name).join(', ')
+                     : '--'
+                  }
+               </p>
+            </React.Fragment>
+         )
+      }
+   };
+
    return (
       <div className="detail-content">
          <div className="header-content" style={{ backgroundImage: `url(${imgURL + movieDetail.backdrop_path})` }}>
             <button className="button btn-go-back" onClick={() => props.history.goBack()}>
-               <Icon.ArrowLeft /> <span>Back</span>
+               <Icon.ArrowLeft /> <span>Back to main</span>
             </button>
             <div className="columns is-multiline">
                <div className="column is-3 card-poster">
@@ -46,22 +81,26 @@ export default function MovieDetail(props) {
                            ) : null}
                         </ul>
                      </nav>
-                     <h5 className="text-title is-size-5">Overview</h5>
-                     <div className="overview">
-                        <p>{movieDetail.overview}</p>
-                     </div>
                      <div className="info-additional">
                         <div className="item rating">
-                           <Icon.Star className="symbol rating" color="#f1c40f" fill="#f1c40f" /> 
+                           <Icon.Star className="symbol rating" color="#f1c40f" fill="#f1c40f" />
                            <span>{movieDetail.vote_average}</span>
                         </div>
                         <div className="item runtime">
                            <Icon.Clock className="symbol" /> <span>{timeConvert(movieDetail.runtime)}</span>
                         </div>
+                        <div className="item">
+                           <button className="button is-rounded">
+                              <Icon.Play fill="#002068" color="#002068" /> Watch Trailer
+                           </button>
+                        </div>
                      </div>
-                     <button className="button is-rounded">
-                        <Icon.Play fill="#002068" color="#002068" /> Watch Trailer
-                     </button>
+                     <h5 className="text-title is-size-5">Overview</h5>
+                     <div className="overview">
+                        <p>{movieDetail.overview}</p>
+                     </div>
+                     {renderCredits()}
+                     {renderProductions()}
                   </div>
                </div>
             </div>
