9a478b27d514edb6db6fec559c13eee2ed4d761a
ahmadertarizqi <ahmadderta.rizqi30@gmail.com>
2020-12-14 23:11:20 +0700

    poster image default

--- src/components/Poster/PeoplePoster.js
+++ src/components/Poster/PeoplePoster.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { imgURL } from 'contants';
+import PersonImgPlaceholder from 'assets/user-placeholder.png';
 
 export default function PeoplePoster({ peopleID, profileImage, name }) {
    return (
       <div className="card-poster is-people">
-         <img src={`${imgURL}/w185${profileImage}`} alt={name} />
+         {profileImage 
+            ? <img src={`${imgURL}/w185${profileImage}`} alt={name} />
+            : <img src={PersonImgPlaceholder} alt="img-default" />
+         }
          <div className="poster-info-wrapper">
             <h5 className="is-size-6 cp-title">{name}</h5>
          </div>
